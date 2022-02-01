8c3373ed175f65a2f4ac4bb8d40023b8c9dc87fb
Giovanni Licari <55797712+giovannilicari@users.noreply.github.com>
2022-02-01 13:03:38 +0100

    Create memcheck.php

---
+++ memcheck.php
@@ -0,0 +1,13 @@
+//memcheck.php
+/**
+ * Gets peak memory usage of a process in KiB from /proc.../status.
+ *
+ * @return int|bool VmPeak, value in KiB. False if data could not be found.
+ */
+function processPeakMemUsage()
+{
+    $status = file_get_contents('/proc/' . getmypid() . '/status'); 
+    $matches = array();
+    preg_match_all('/^(VmPeak):\s*([0-9]+).*$/im', $status, $matches);  
+    return !isset($matches[2][0]) ? false : intval($matches[2][0]);
+}
